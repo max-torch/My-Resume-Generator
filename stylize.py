30843ad747ac1bb353cb57272ed7117d4dbb4716
Alexander Lacson <41433185+max-torch@users.noreply.github.com>
2021-12-11 04:28:52 +0800

    Run yapf on stylize.py

--- stylize.py
+++ stylize.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import pdfkit
 
-soup = BeautifulSoup(open("Resume.html", encoding = 'utf8'), "html.parser")
+soup = BeautifulSoup(open("Resume.html", encoding='utf8'), "html.parser")
 headings = soup.find_all('h3')
 
 for heading in headings:
@@ -16,5 +16,6 @@ soup.head.append(new_link)
 with open("StylizedResume.html", "wb") as f_output:
     f_output.write(soup.prettify("utf-8"))
 
-config = pdfkit.configuration(wkhtmltopdf="C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe")
+config = pdfkit.configuration(
+    wkhtmltopdf="C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe")
 pdfkit.from_file('StylizedResume.html', 'My Resume.pdf', configuration=config)
