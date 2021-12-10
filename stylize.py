fe459632a6cb3151082d31922631f90f2fc1386c
Alexander Lacson <41433185+max-torch@users.noreply.github.com>
2021-12-11 03:13:46 +0800

    Automate generation of pdf from VSCode HTML ouptut

---
+++ stylize.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import pdfkit
+
+soup = BeautifulSoup(open("Resume.html", encoding = 'utf8'), "html.parser")
+headings = soup.find_all('h3')
+
+for heading in headings:
+    new_span = soup.new_tag("span")
+    for content in reversed(heading.contents):
+        new_span.insert(0, content.string)
+    heading.append(new_span)
+
+new_link = soup.new_tag("link", rel="stylesheet", href="styles.css")
+soup.head.append(new_link)
+
+with open("StylizedResume.html", "wb") as f_output:
+    f_output.write(soup.prettify("utf-8"))
+
+config = pdfkit.configuration(wkhtmltopdf="C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe")
+pdfkit.from_file('StylizedResume.html', 'Alexander Lacson Resume.pdf', configuration=config)
